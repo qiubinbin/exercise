cb6271fd0cf93948584e3f6942a0e548d40fa15b
qiubinbin <qiubin780061109@163.com>
2017-12-19 00:13:42 +0800

    2017年12月18日23:34:44

--- temp1.py
+++ temp1.py
@@ -1,2 +1,10 @@
-a=[1,2,3,4]
-print(a[-11:20])
+import itertools
+
+a = [1, 2, 3, 4, 1, 2, 5, 6, 7, 4, 2, 1]
+a.sort()
+b='qiubin'
+temp=list(b)
+print(temp)
+temp.sort()
+for m, n in itertools.groupby(temp):
+    print(m)
